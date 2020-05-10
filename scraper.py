e44a859df9dafb07021044f694d33c0b9530cd8a
Andrew Wang <andrew.zi.wang7@gmail.com>
2020-05-10 01:18:57 -0700

    latex formatting

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import re, pyperclip
 
 regexScrape = re.compile(r'''
-[A-Z]    # begin with cpaital letter
-.*
+[A-Z]    # begin with capital letter
+.* 
 \\it\s
 .*
 \.
@@ -19,9 +19,44 @@ regexPrim = re.compile(r'''
 text = pyperclip.paste()
 exstractedLines = regexScrape.findall(text)
 
-text_file = open("latexFile.txt", "wt")
+text_file = open("latexOutput.txt", "wt")
+text_top = """\\documentclass{article}
+\\usepackage{amssymb, mathrsfs, enumitem}
+\\usepackage{tikz-cd}
+\\usepackage{multicol}
+\\usepackage{graphicx}
+\\usepackage{color}
+\\usepackage{amsthm}
+\\usepackage{hyperref}
+\\usepackage{verbatim}
+\\usepackage{caption}
+\\usepackage{subcaption}
+\\usepackage{amsmath, epsfig, colortbl}
+\\usepackage[breakable, skins]{tcolorbox}
+\\setlength{\parskip}{8pt}
+\\usepackage{geometry}
+\\geometry{
+ a4paper,
+ total={170mm,257mm},
+ left=20mm,
+ top=20mm,
+ }
+\\begin{document}
+\\section{Important Terms}
+\\begin{itemize}
+\n"""
+
+text_bot = '''
+\end{itemize}
+\end{document}'''
+
+text_add = '\\item '
+
+text_file.write(text_top)
 
 for line in exstractedLines:
-    text_file.write(line + '\n')
+    text_file.write(text_add + line + '\n')
+
+text_file.write(text_bot)
 
 text_file.close()
